773a6b35fd8ab0d1dba4237c736608706df2d888
Mukhammedali <muhanai1913@gmail.com>
2023-07-09 10:48:32 +0600

    fixed a typo to test project at deployment service

--- components/Layout/Header/Actions/ActionsUnAuth.jsx
+++ components/Layout/Header/Actions/ActionsUnAuth.jsx
@@ -1,6 +1,6 @@
 import ButtonViolet from "../../../UI/Buttons/ButtonViolet";
 
-import styles from "./ActionsUnauth.module.css";
+import styles from "ActionsUnAuth.module.css";
 const ActionUnauth = ({ Link }) => {
   return (
     <div className={styles.unAuthActions}>
